d15ec435e1d4ba4043907b2d7e0da87aa6b0925a
Daniel Umberto <danielumberto51@gmail.com>
2024-06-12 14:54:03 -0300

    USERID

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -6,24 +6,22 @@ public class Contact {
 	private String name;
 	private String phone;
 	private String email;
+	private int userID;
 
-
-	public Contact(int id, String name, String phone, String email) {
+	public Contact(int id, String name, String phone, String email, int userID) {
 		this.id = id;
 		this.name = name;
 		this.phone = phone;
 		this.email = email;
+		this.userID = userID;
 	}
 
-
-
-	public Contact(int id, String name, String phone) {
+	public Contact(int id, String name, String phone, int userID) {
 		this.id = id;
 		this.name = name;
 		this.phone = phone;
+		this.userID = userID;
 	}
-
-
 
 
 	public int getId() {
@@ -31,20 +29,14 @@ public class Contact {
 	}
 
 
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
 
 
-
-
 	public String getName() {
 		return name;
 	}
-
-
 
 
 	public void setName(String name) {
@@ -57,28 +49,31 @@ public class Contact {
 	}
 
 
-
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
-
-
+	
 
 	public String getEmail() {
 		return email;
 	}
 
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
+	
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
 	public String toString() {
-		return " |ID: " +id+ " |Nome: " +name+ " |Telefone: " +phone+ " |E-mail: " +email;
+		return " |ID: " +id+ " |Nome: " +name+ " |Telefone: " +phone+ " |E-mail: " +email+ " |ID do usuário:" +userID;
 	}
 	
 }
