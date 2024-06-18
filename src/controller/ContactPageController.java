ac5868781b3795c16d973844c57e4bf708e47e21
Daniel Umberto <danielumberto51@gmail.com>
2024-06-18 14:40:37 -0300

    boas praticas

--- src/controller/ContactPageController.java
+++ src/controller/ContactPageController.java
@@ -22,150 +22,57 @@ import javafx.scene.Scene;
 import model.Contact;
 import util.AlertMessage;
 
+public class ContactPageController {
 
-public class ContactPageController{
+    @FXML
+    private Button contactPageDeleteButton;
 
-	 @FXML
-	 private Button contactPageDeleteButton;
+    @FXML
+    private TextField contactPageEmail;
 
-	 @FXML
-	 private TextField contactPageEmail;
+    @FXML
+    private TableColumn<Contact, String> contactPageEmailColumn;
 
-	 @FXML
-	 private TableColumn<Contact, String> contactPageEmailColumn;
+    @FXML
+    private TableColumn<Contact, Integer> contactPageIDColumn;
 
-	 @FXML
-	 private TableColumn<Contact, Integer> contactPageIDColumn;
+    @FXML
+    private TextField contactPageName;
 
-	 @FXML
-	 private TextField contactPageName;
+    @FXML
+    private TableColumn<Contact, String> contactPageNameColumn;
 
-	 @FXML
-	 private TableColumn<Contact, String> contactPageNameColumn;
+    @FXML
+    private TextField contactPagePhone;
 
-	 @FXML
-	 private TextField contactPagePhone;
+    @FXML
+    private TableColumn<Contact, String> contactPagePhoneColumn;
 
-	 @FXML
-	 private TableColumn<Contact, String> contactPagePhoneColumn;
+    @FXML
+    private Button contactPageSaveButton;
 
-	 @FXML
-	 private Button contactPageSaveButton;
+    @FXML
+    private Button contactPageUpdateButton;
 
-	 @FXML
-	 private Button contactPageUpdateButton;
+    @FXML
+    private TableView<Contact> contactPageTable;
 
-	 @FXML
-	 private TableView<Contact> contactPageTable;
-
-    
-    //List para contatos
     private ObservableList<Contact> contacts = FXCollections.observableArrayList();
-    
- // ID do usuário que está usando a aplicação
     private int userID;
 
- // Método para definir o ID do usuário e carregar a tabela de contatos
+    // Método para definir o ID do usuário e carregar a tabela de contatos
     public void setUserId(int userID) {
         this.userID = userID;
         table();
     }
 
-
     // Método para adicionar um novo contato
-	@FXML
+    @FXML
     public void addContact(ActionEvent event) {
-    	 AlertMessage alert = new AlertMessage();  
-
-    	    try {
-    	        String name = contactPageName.getText().trim();
-    	        String phone = contactPagePhone.getText().trim();
-    	        String email = contactPageEmail.getText().trim();
-
-    	        // Verifica se os campos obrigatórios estão preenchidos
-    	        if (name.isEmpty() || phone.isEmpty()) {
-    	            alert.errorMessage("Preencha os campos necessários!");
-    	            return;
-    	        }
-    	        
-    	        //Instancia ContactDAO
-    	        ContactDAO contactDAO = new ContactDAO();
-    	        //Cria novo objeto Contact
-    	        Contact contact = new Contact(name, phone, email, userID);
-    	        
-
-    	        //Adiciona contato no banco de dados!
-    	        contactDAO.addContact(contact);  
-    	        alert.successMessage("Contato adicionado com sucesso!");
-    	        //Limpa caixa de texto
-    	        clearFields();
-    	        //Atualiza tabela
-    	        table();
-    	    } catch (SQLException e) {
-    	        e.printStackTrace();
-    	        alert.errorMessage("Ocorreu um erro ao adicionar o contato.");
-    	    } catch (Exception e) {
-    	        e.printStackTrace();
-    	        alert.errorMessage("Erro inesperado ao adicionar o contato.");
-    	    }
-    }
-    
-	//Limpando campos de texto!
-    private void clearFields() {
-        contactPageName.setText("");
-        contactPagePhone.setText("");
-        contactPageEmail.setText("");
-    }
-    
-    // Método para carregar a tabela de contatos
-    public void table() {
-    	// Define as propriedades das colunas
-    	 contactPageIDColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-         contactPageNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-         contactPagePhoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
-         contactPageEmailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
-
-         // Configura um evento de clique nas linhas da tabela
-         contactPageTable.setRowFactory(tv -> {
-             TableRow<Contact> row = new TableRow<>();
-             row.setOnMouseClicked(event -> {
-                 if (event.getClickCount() == 1 && (!row.isEmpty())) {
-                     Contact contact = row.getItem();
-                     contactPageName.setText(contact.getName());
-                     contactPagePhone.setText(contact.getPhone());
-                     contactPageEmail.setText(contact.getEmail());
-                 }
-             });
-             return row;
-         });
-
-         
-         ContactDAO contactDAO = new ContactDAO();
-         try {
-             contacts = contactDAO.getContacts(userID);
-             contactPageTable.setItems(contacts);
-         } catch (SQLException e) {
-             e.printStackTrace();
-             new AlertMessage().errorMessage("Erro ao carregar contatos: " + e.getMessage());
-         }
-     }
-
-    //Método para fechar a tela
-    @FXML
-    public void close(MouseEvent event) {
-    	 Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-         window.close();
-    }
-
-    @FXML
-    public void updateContact(ActionEvent event) {
         AlertMessage alert = new AlertMessage();
-
         try {
-        	//Selecionando o contato
-            Contact selectedContact = contactPageTable.getSelectionModel().getSelectedItem();
-            if (selectedContact == null) {
-                alert.errorMessage("Selecione um contato para atualizar.");
+            if (!checkRequiredFields()) {
+                alert.errorMessage("Preencha os campos necessários!");
                 return;
             }
 
@@ -173,28 +80,98 @@ public class ContactPageController{
             String phone = contactPagePhone.getText().trim();
             String email = contactPageEmail.getText().trim();
 
-            if (name.isEmpty() || phone.isEmpty()) {
+            ContactDAO contactDAO = new ContactDAO();
+            Contact contact = new Contact(name, phone, email, userID);
+            contactDAO.addContact(contact);
+            alert.successMessage("Contato adicionado com sucesso!");
+
+            clearFields();
+            table();
+        } catch (SQLException e) {
+            handleException(alert, e, "Ocorreu um erro ao adicionar o contato.");
+        } catch (Exception e) {
+            handleException(alert, e, "Erro inesperado ao adicionar o contato.");
+        }
+    }
+
+    // Método para verificar se os campos obrigatórios estão preenchidos
+    private boolean checkRequiredFields() {
+        return !contactPageName.getText().trim().isEmpty() && !contactPagePhone.getText().trim().isEmpty();
+    }
+
+    // Método para carregar a tabela de contatos
+    private void table() {
+        contactPageIDColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        contactPageNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        contactPagePhoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        contactPageEmailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+        contactPageTable.setRowFactory(tv -> {
+            TableRow<Contact> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 1 && (!row.isEmpty())) {
+                    Contact contact = row.getItem();
+                    populateFields(contact);
+                }
+            });
+            return row;
+        });
+
+        ContactDAO contactDAO = new ContactDAO();
+        try {
+            contacts = contactDAO.getContacts(userID);
+            contactPageTable.setItems(contacts);
+        } catch (SQLException e) {
+            handleException(new AlertMessage(), e, "Erro ao carregar contatos: ");
+        }
+    }
+
+    // Método para preencher os campos de texto com as informações do contato selecionado
+    private void populateFields(Contact contact) {
+        contactPageName.setText(contact.getName());
+        contactPagePhone.setText(contact.getPhone());
+        contactPageEmail.setText(contact.getEmail());
+    }
+
+    // Método para limpar os campos de texto
+    private void clearFields() {
+        contactPageName.setText("");
+        contactPagePhone.setText("");
+        contactPageEmail.setText("");
+    }
+
+    // Método para atualizar um contato
+    @FXML
+    public void updateContact(ActionEvent event) {
+        AlertMessage alert = new AlertMessage();
+        try {
+            Contact selectedContact = contactPageTable.getSelectionModel().getSelectedItem();
+            if (selectedContact == null) {
+                alert.errorMessage("Selecione um contato para atualizar.");
+                return;
+            }
+
+            if (!checkRequiredFields()) {
                 alert.errorMessage("Preencha os campos necessários!");
                 return;
             }
 
-            //Selecionando o nome, telefone e email
+            String name = contactPageName.getText().trim();
+            String phone = contactPagePhone.getText().trim();
+            String email = contactPageEmail.getText().trim();
+
             selectedContact.setName(name);
             selectedContact.setPhone(phone);
             selectedContact.setEmail(email);
 
-            //Atualizando o contato
             ContactDAO contactDAO = new ContactDAO();
             contactDAO.updateContact(selectedContact);
 
             alert.successMessage("Contato atualizado com sucesso!");
-            //Limpando caixas de texto
             clearFields();
-            //Atualizando tabela
             table();
         } catch (SQLException e) {
-            e.printStackTrace();
-            alert.errorMessage("Ocorreu um erro ao atualizar o contato.");
+            handleException(alert, e, "Ocorreu um erro ao atualizar o contato.");
         }
     }
 
@@ -202,44 +179,57 @@ public class ContactPageController{
     @FXML
     public void deleteContact(ActionEvent event) {
         AlertMessage alert = new AlertMessage();
-
         try {
-        	//Selecionando um contato
             Contact selectedContact = contactPageTable.getSelectionModel().getSelectedItem();
             if (selectedContact == null) {
                 alert.errorMessage("Selecione um contato para deletar.");
                 return;
             }
-            
-            //Através do ID eu deleto o contato!
+
             int contactId = selectedContact.getId();
             ContactDAO contactDAO = new ContactDAO();
             contactDAO.deleteContact(contactId);
 
             alert.successMessage("Contato deletado com sucesso!");
-            //Limpando caixas de texto
             clearFields();
-            //Atualizando tabela
             table();
         } catch (SQLException e) {
-            e.printStackTrace();
-            alert.errorMessage("Ocorreu um erro ao deletar o contato.");
+            handleException(alert, e, "Ocorreu um erro ao deletar o contato.");
         }
     }
 
- // Método para navegar para a página do usuário
+    // Método para fechar a tela e redirecionar para a tela de login
+    @FXML
+    public void close(MouseEvent event) {
+        try {
+            Parent view = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
+            Scene scene = new Scene(view);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para navegar para a página do usuário
     @FXML
     public void clickedUser(MouseEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/UserPage.fxml"));
         Parent view = loader.load();
-        
-        // Passando o ID do usuário para o UserPageController
+
         UserPageController userPageController = loader.getController();
         userPageController.setUserId(userID);
-        
+
         Scene scene = new Scene(view);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.show();
+    }
+
+    // Método para lidar com exceções e mostrar uma mensagem de erro ao usuário
+    private void handleException(AlertMessage alert, Exception e, String message) {
+        e.printStackTrace();
+        alert.errorMessage(message + e.getMessage());
     }
 }
