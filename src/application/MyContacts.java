bf847f3eea466702d8eb3870f8af5bc1fd6f4c03
Daniel Umberto <danielumberto51@gmail.com>
2024-06-18 21:16:42 -0300

    boas praticas

--- src/application/MyContacts.java
+++ src/application/MyContacts.java
@@ -17,42 +17,37 @@ public class MyContacts extends Application {
     // Método inicial do aplicativo JavaFX
     @Override
     public void start(Stage primaryStage) {
-        // Armazenar o palco principal do aplicativo
         this.primaryStage = primaryStage;
 
-        // Exibir a janela principal
+        
         mainWindow();
     }
 
     // Método principal para carregar e exibir a janela de login
     public void mainWindow() {
         try {
-            // Carregando o arquivo FXML da tela de login
+           
             FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/view/LoginScreen.fxml"));
             AnchorPane pane = loader.load();
 
-            // Carregando e referenciando a classe controller da tela de login
+            
             LoginScreenController mainWindowController = loader.getController();
             mainWindowController.setMain(this);
 
-            // Criando uma nova cena com o layout carregado
+            
             Scene scene = new Scene(pane);
-            // Configurando a cena no palco principal
+            
             primaryStage.setScene(scene);
-            // Definindo o estilo do palco principal como não decorado (sem barra de título padrão)
             primaryStage.initStyle(StageStyle.UNDECORATED);
-            // Exibindo o palco principal
             primaryStage.show();
 
         } catch (IOException e) {
-            // Imprimindo o stack trace em caso de erro ao carregar o arquivo FXML
             e.printStackTrace();
         }
     }
 
     // Método principal para lançar o aplicativo
     public static void main(String[] args) {
-        // Método estático launch da classe Application para iniciar o aplicativo JavaFX
         launch(args);
     }
 }
--- src/dao/ConnectDB.java
+++ src/dao/ConnectDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectDB {
 
    
-    private static final String URL = "jdbc:mysql://localhost:3306/mydb?useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8";
+    private static final String URL = "jdbc:mysql://localhost:3306/AGENDA?useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8";
     private static final String USER = "root";
     private static final String PASSWORD = "soutodrex123";
 
