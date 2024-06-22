96a7bc83923a1ea3d91a2ed834fc2d1a7e02caec
Daniel Umberto <danielumberto51@gmail.com>
2024-06-22 17:49:26 -0300

    solucao de erros

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -2,18 +2,18 @@ package model;
 
 public class Contact {
 
-	//Atributos
+	
 	private int id;
 	private String name;
 	private String phone;
 	private String email;
 	private int userID;
 	
-	//Construtor
+	
 	public Contact() {
 	}
 
-	//Construtor
+	
 	public Contact(int id, String name, String phone, String email, int userID) {
 		this.id = id;
 		this.name = name;
@@ -22,7 +22,7 @@ public class Contact {
 		this.userID = userID;
 	}
 
-	//Construtor
+	
 	public Contact(String name, String phone, String email, int userID) {
 		this.name = name;
 		this.phone = phone;
@@ -32,62 +32,61 @@ public class Contact {
 
 
 
-	//Retornar o ID
+	
 	public int getId() {
 		return id;
 	}
 	
-	//Mudar o ID
+	
 	public void setId(int id) {
 		this.id = id;
 	}
 
 
-	//Retornar o nome
+	
 	public String getName() {
 		return name;
 	}
 
-	//Mudar o nome
+	
 	public void setName(String name) {
 		this.name = name;
 	}
 	
 	
-	//Retornar o telefone
+	
 	public String getPhone() {
 		return phone;
 	}
 
-	//Mudar o telefone
+	
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 	
-	//Retornar o Email
+	
 	public String getEmail() {
 		return email;
 	}
 
-	//Mudar o Email
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
-	//Retornar o ID do usuário que pertence
+	
 	public int getUserID() {
 		return userID;
 	}
 
-	//Mudar o ID do usuário que pertence
+	
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
 
-	//Sobrescrita do método toString
+	
 	@Override
 	public String toString() {
-		//Retorna os dados completos!
 		return " |ID: " +id+ " |Nome: " +name+ " |Telefone: " +phone+ " |E-mail: " +email+ " |ID do usuário:" +userID;
 	}
 	
--- src/model/User.java
+++ src/model/User.java
@@ -2,7 +2,7 @@ package model;
 
 public class User {
 	
-	//Atributos
+	
 	private int id;
 	private String name;
 	private String email;
@@ -10,11 +10,11 @@ public class User {
 	private String question;
 	private String answer;
 	
-	//Construtor
+	
 	public User() {
 	}
 	
-	//Construtor
+	
 	public User(int id, String name, String email, String password, String question, String answer) {
 		this.id = id;
 		this.name = name;
@@ -24,7 +24,7 @@ public class User {
 		this.answer = answer;
 	}
 	
-	//Construtor
+	
 	public User(String name, String email, String password, String question, String answer) {
 		this.name = name;
 		this.email = email;
@@ -33,72 +33,70 @@ public class User {
 		this.answer = answer;
 	}
 	
-	//Método para retornar o ID
+	
 	public int getId() {
 		return id;
 	}
 
-	//Método para mudar o ID
+	
 	public void setId(int id) {
 		this.id = id;
 	}
 	
-	//Método para retornar o nome
+	
 	public String getName() {
 		return name;
 	}
 
-	//Método para mudar o nome
+	
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	//Método para retornar o email
+	
 	public String getEmail() {
 		return email;
 	}
 
-	//Método para mudar o email
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	//Método para retornar a senha
+	
 	public String getPassword() {
 		return password;
 	}
 
-	//Método para mudar a senha
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
 	
-	//Método para retornar a alternativa
+	
 	public String getQuestion() {
 		return question;
 	}
 
-	//Método para mudar a alternativa
+	
 	public void setQuestion(String question) {
 		this.question = question;
 	}
 
-	//Método para retornar a resposta
+	
 	public String getAnswer() {
 		return answer;
 	}
 
-	//Método para mudar a resposta
+	
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
 
 
-	//Sobrescrita do método toString!
 	@Override
 	public String toString() {
-		//Retornar todos os dados
 		return " |ID: " +id+ " |Nome: " +name+ " |E-mail: " +email+ " |Senha: " +password+ ""
 			+ " |Pergunta: " +question+ " |Resposta: " +answer;
 	}
